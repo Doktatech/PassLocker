42eefc43877992e37fc6eab804117e1e2ff6ec54
Doktatech <Doktatech2@gmail.com>
2019-05-13 01:33:27 +0300

    Create a second test for saving users

--- test_user.py
+++ test_user.py
@@ -9,5 +9,11 @@ class TestUser (unittest.TestCase):
         First test to check if object is being initialized
         """
         self.new_user= User ("Instagram", "Duke", "12345")
+    def test_save_user(self):
+        """
+        Test if the app is saving users into the list
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
 if __name__ == '__main__':
     unittest.main()
