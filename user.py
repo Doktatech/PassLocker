f5c2d087247d2f23ac4c496cc399d2093660fb4d
Doktatech <Doktatech2@gmail.com>
2019-05-13 01:27:25 +0300

    Modify objects instance variable

--- user.py
+++ user.py
@@ -1,6 +1,10 @@
 class User :
-     """
-    Class that generates new instances of users
+    """
+    Class that generates new instances of users.
     """
     user_list =[]
-def _init_ (self,account,username,password):
+    def _init_ (self,account,username,password):
+        self.account =account
+        self.username=username
+        self.password=password
+    
