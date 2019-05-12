240620ee13604e92169a72960e8821cba37581c1
Doktatech <Doktatech2@gmail.com>
2019-05-13 01:28:05 +0300

    Create the first setuP test

--- test_user.py
+++ test_user.py
@@ -1,5 +1,13 @@
-import unitest
+import unittest
 from user import User 
-class TestUser (unitest.TestCase):
+class TestUser (unittest.TestCase):
     """
     Test class that defines test cases for the user class behaviours.
+    """
+    def setUp (self):
+        """
+        First test to check if object is being initialized
+        """
+        self.new_user= User ("Instagram", "Duke", "12345")
+if __name__ == '__main__':
+    unittest.main()
