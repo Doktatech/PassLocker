992d28c9daf41e7bbd5c297708bfc632e2ec83a3
Doktatech <Doktatech2@gmail.com>
2019-05-13 13:46:43 +0300

    Define functions to check user exists and display users

--- display.py
+++ display.py
@@ -10,4 +10,20 @@ def save_user(user):
     """
     Function for saving user
     """
-    
+   user.save_user()
+def find_user(account):
+    """
+    Function for finding user using username
+    """
+    return User.find_by_username(username)
+def check_existing_user(account):
+    """
+    Function for checking if the user exists
+    """
+    return User.user_exists(account)
+def display_users():    
+    """
+    Function for displaying users
+    """
+
+    return User.display_users() 
