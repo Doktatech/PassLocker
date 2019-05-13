8b2fd86600ecdc37f44ccbbf36044c848175d5df
Doktatech <Doktatech2@gmail.com>
2019-05-13 10:28:01 +0300

    Create a method for saving  a user

--- user.py
+++ user.py
@@ -3,7 +3,7 @@ class User :
     Class that generates new instances of users.
     """
     user_list =[]
-    def _init_ (self,account,username,password):
+    def __init__ (self,account,username,password):
         self.account =account
         self.username=username
         self.password=password
